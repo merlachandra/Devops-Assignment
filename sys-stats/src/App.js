946ba6952d93ace69266fdf9b2b6c49893bc7f9c
chandrababumerla <chandrababu.merla@cumulocity.com>
2022-03-28 09:32:01 +0530

    updated port to be consistent with k8s deployment

--- sys-stats/src/App.js
+++ sys-stats/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   async loadData() {
     try {
-      const res = await fetch('http://localhost:81/stats');
+      const res = await fetch('http://localhost:30100/stats');
       const blocks = await res.json();
       const ram = blocks.ram;
       const cpu = blocks.cpu;
